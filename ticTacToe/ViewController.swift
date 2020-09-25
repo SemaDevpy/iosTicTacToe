108a49b3c5490b82bc5881d5878911ef28d2d792
SemaDevpy <syimykdev@gmail.com>
2020-09-25 19:10:37 +0600

    Initial Commit

---
+++ ticTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by Syimyk on 9/25/20.
+//  Copyright © 2020 Syimyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
